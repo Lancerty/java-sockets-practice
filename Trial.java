8da184c3ec0f89353af566367e46b7e07fa7f11f
Lancerty <lancebarana@yahoo.com>
2024-01-18 12:47:49 +0800

    add: multiple java class files

---
+++ Trial.java
@@ -0,0 +1,11 @@
+import java.net.*;
+import java.io.*;
+
+public class Trial {
+    public static void main(String[] args) throws IOException {
+        
+    }
+    static class Server {
+        
+    }
+}
